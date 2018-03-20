cd84e5165ee896ec60251573384db63e3a81a591
Aniket <aniketengg.225@gmail.com>
2018-03-21 00:41:54 +0530

    metamask interaction introduced

---
+++ src/app/services/web3.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+import * as Web3 from 'web3';
+
+declare var window: any;
+
+@Injectable()
+export class Web3Service {
+
+	public web3: any;
+
+  constructor() { 
+  	this.checkAndInstantiateWeb3();
+  }
+
+  checkAndInstantiateWeb3 = () => {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.web3 !== 'undefined') {
+      console.warn(
+        'Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask'
+      );
+      // Use Mist/MetaMask's provider
+      this.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.warn(
+        'No web3 detected.'
+      );
+    }
+  };
+
+  getAccounts = () =>{
+      this.web3.eth.getAccounts((err, accs) => {
+            if (err != null) {
+                alert('There was an error fetching your accounts.');
+            }
+            if (accs.length === 0) {
+                alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
+            }
+                alert(accs);
+            });
+            } 
+}
